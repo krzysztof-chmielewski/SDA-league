31cf5aac450ca9a8e856b7ab9cb6b41484c7cddf
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-12-02 19:55:17 +0100

    Retrofitted cleaning test directories to actually work

--- src/test/java/com/kchmielewski/sda/league/importing/DirectoriesCleanup.java
+++ src/test/java/com/kchmielewski/sda/league/importing/DirectoriesCleanup.java
@@ -1,31 +1,27 @@
 package com.kchmielewski.sda.league.importing;
 
-import com.google.common.io.Files;
 import org.junit.After;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
-public abstract class FileCleanupTest {
+public abstract class DirectoriesCleanup {
     private final String importDirectory = "src/test/resources/import/";
     private final String processedDirectory = "src/test/resources/processed";
     private final String errorDirectory = "src/test/resources/error";
-
+    private final List<String> directoriesToClear = Arrays.asList(importDirectory, processedDirectory, errorDirectory);
 
     @After
     public void tearDown() throws Exception {
-        for (String directoryName : Arrays.asList(errorDirectory, processedDirectory)) {
-
+        for (String directoryName : directoriesToClear) {
             File directory = new File(directoryName);
             File[] files = directory.listFiles();
             if (files != null) {
                 Stream.of(files).forEach(f -> {
-                    try {
-                        Files.move(f, new File(importDirectory + f.getName()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    if (!f.delete()) {
+                        throw new RuntimeException("There was a problem deleting file: " + f);
                     }
                 });
             }
